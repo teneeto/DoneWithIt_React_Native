a1299d786d9680f6b2339995fe6a66f202ef7e0c
Olei Eto <teneeto@gmail.com>
2020-11-28 09:18:06 +0100

    add View Image Screen

--- App.js
+++ App.js
@@ -1,12 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import ViewImageScreen from './app/screens/ViewImageScreen';
 import WelcomeScreen from './app/screens/WelcomeScreen';
 
 export default function App() {
   let x = 1;
   console.log('App executed');
   return (
-    <WelcomeScreen />
+    <ViewImageScreen />
   );
 }
