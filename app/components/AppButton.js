ba9a0c6eb26ccced84e768fcba11dbec68856e31
Olei Eto <teneeto@gmail.com>
2020-11-28 12:09:35 +0100

    update welcome screen

--- app/components/AppButton.js
+++ app/components/AppButton.js
@@ -21,6 +21,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     padding: 15,
     width: '100%',
+    marginVertical: 10,
   },
   text: {
     color: colors.white,
--- app/screens/WelcomeScreen.js
+++ app/screens/WelcomeScreen.js
@@ -7,11 +7,12 @@ import colors from '../config/colors'
 function WelcomeScreen(props) {
   return (
     <ImageBackground
+      blurRadius={10}
       style={styles.background}
       source={require('../assets/background.jpg')}>
       <View style={styles.logoContainer}>
         <Image style={styles.logo} source={require('../assets/logo-red.png')} />
-        <Text>Sell What You Don't Need</Text>
+        <Text style={styles.tagline}>Sell What You Don't Need</Text>
       </View>
 
       <View style={styles.buttonsContainer}>
@@ -43,5 +44,10 @@ const styles = StyleSheet.create({
     top: 70,
     alignItems: 'center'
   },
+  tagline: {
+    fontSize: 25,
+    fontWeight: '600',
+    paddingVertical: 20,
+  }
 })
 export default WelcomeScreen;
