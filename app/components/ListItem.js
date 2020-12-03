30713c5e9105d9efbde5ce7599b7355a72cd42bf
Olei Eto <teneeto@gmail.com>
2020-12-03 10:46:14 +0100

    add delete Action button

--- app/components/ListItem.js
+++ app/components/ListItem.js
@@ -1,18 +1,27 @@
 import React from 'react';
-import { StyleSheet, View, Image } from 'react-native';
+import { StyleSheet, View, Image, TouchableHighlight } from 'react-native';
+import { Swipeable } from 'react-native-gesture-handler';
+
 import AppText from './AppText';
 
 import colors from '../config/colors';
 
-function ListItem({ title, subTitle, image }) {
+function ListItem({ title, subTitle, image, onPress, renderRightActions }) {
   return (
-    <View style={styles.container}>
-      <Image style={styles.image} source={image} />
-      <View>
-        <AppText style={styles.title} >{title}</AppText>
-        <AppText style={styles.subTitle}>{subTitle}</AppText>
-      </View>
-    </View >
+    <Swipeable renderRightActions={renderRightActions}>
+      <TouchableHighlight
+        underlayColor={colors.light}
+        onPress={onPress}
+      >
+        <View style={styles.container}>
+          <Image style={styles.image} source={image} />
+          <View>
+            <AppText style={styles.title} >{title}</AppText>
+            <AppText style={styles.subTitle}>{subTitle}</AppText>
+          </View>
+        </View >
+      </TouchableHighlight>
+    </Swipeable>
   );
 }
 const styles = StyleSheet.create({
--- app/screens/MessagesScreen.js
+++ app/screens/MessagesScreen.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import { FlatList, StyleSheet } from 'react-native';
-
+import { FlatList, StyleSheet, View } from 'react-native';
 import ListItem from '../components/ListItem';
+import ListItemDeleteAction from '../components/ListItemDeleteAction';
 import ListItemSeparator from '../components/ListItemSeparator';
 import Screen from '../components/Screen'
 
@@ -31,9 +31,10 @@ function MessagesScreen(props) {
             title={item.title}
             subTitle={item.description}
             image={item.image}
+            onPress={() => console.log('Message Selected', item)}
+            renderRightActions={() => <ListItemDeleteAction onPress={() => console.log(item)} />}
           />
           )}
-
         ItemSeparatorComponent={ListItemSeparator}
       />
 
