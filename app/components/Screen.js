436cb0ca79eb2758c5181b4e2e047b45ef5f4d64
Olei Eto <teneeto@gmail.com>
2020-12-05 23:59:29 +0100

    update Screen

--- app/components/Screen.js
+++ app/components/Screen.js
@@ -5,7 +5,7 @@ import Constants from 'expo-constants';
 function Screen({ children, style }) {
   return (
     <SafeAreaView style={[styles.screen, style]}>
-      <View style={style}>
+      <View style={[style, styles.view]}>
         {children}
       </View>
     </SafeAreaView>
@@ -15,6 +15,9 @@ const styles = StyleSheet.create({
   screen: {
     // paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0,
     paddingTop: Constants.statusBarHeight,
+    flex: 1
+  },
+  view: {
     flex: 1
   },
 })
