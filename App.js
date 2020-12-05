a84043088d965e756e22399606508db0a952018f
Olei Eto <teneeto@gmail.com>
2020-12-05 23:52:16 +0100

    update list item

--- App.js
+++ App.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import ListItem from './app/components/ListItem';
+import Screen from './app/components/Screen';
 import ListingEditScreen from './app/screens/ListingEditScreen';
 
 import LoginScreen from './app/screens/LoginScreen';
 
 export default function App() {
   return (
-    <ListingEditScreen />
+    <Screen>
+      <ListItem />
+    </Screen>
   );
 }
--- app/components/ListItem.js
+++ app/components/ListItem.js
@@ -1,18 +1,25 @@
 import React from 'react';
-import { StyleSheet, View, Image, TouchableHighlight } from 'react-native';
+import {
+  Image,
+  StyleSheet,
+  TouchableHighlight,
+  View,
+} from 'react-native';
 import { Swipeable } from 'react-native-gesture-handler';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 
 import AppText from './AppText';
-
 import colors from '../config/colors';
 
+
 function ListItem({
-  title,
-  subTitle,
   image,
   IconComponent,
   onPress,
-  renderRightActions }) {
+  renderRightActions,
+  subTitle,
+  title,
+}) {
   return (
     <Swipeable renderRightActions={renderRightActions}>
       <TouchableHighlight
@@ -26,6 +33,10 @@ function ListItem({
             <AppText style={styles.title} >{title}</AppText>
             {subTitle && <AppText style={styles.subTitle}>{subTitle}</AppText>}
           </View>
+          <MaterialCommunityIcons
+            color={colors.medium}
+            name='chevron-right'
+            size={25} />
         </View >
       </TouchableHighlight>
     </Swipeable>
@@ -33,11 +44,13 @@ function ListItem({
 }
 const styles = StyleSheet.create({
   container: {
+    alignItems: 'center',
     flexDirection: 'row',
     padding: 15,
     backgroundColor: colors.white,
   },
   detailsContainer: {
+    flex: 1,
     marginLeft: 10,
     justifyContent: 'center'
   },
